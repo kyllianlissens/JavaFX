efa8dcaf258ff333e513ef11464ca1d5adbc7421
Gevzzz <gevorg.minasyan15@gmail.com>
2021-01-05 13:18:00 +0100

    Added blockshape enums

--- src/main/java/com/company/BlockShape.java
+++ src/main/java/com/company/BlockShape.java
@@ -5,7 +5,14 @@ import java.awt.*;
 public enum BlockShape {
 
     BIG_L (new Point[]{new Point(0,0), new Point(0,-1), new Point(0,-2), new Point(1,-2) }, 3),
-    SMALL_L (new Point[] {new Point(0,0), new Point(0,-1), new Point(1,1)}, 1);
+    SMALL_L (new Point[] {new Point(0,0), new Point(0,-1), new Point(1,1)}, 1),
+    LINEH (new Point[] {new Point(0,0), new Point(1,0), new Point(2,0)}, 1),
+    LINENV (new Point[] {new Point(0,0), new Point(0,-1),  new Point(0,1)}, 1),
+    RB_L (new Point[] {new Point(0,0), new Point(-1,0), new Point(-2,0), new Point(-2,-1)}, 1),
+    RS_L (new Point[] {new Point(0,0), new Point(-1,0), new Point(1,-1)}, 1),
+    TONT (new Point[] {new Point(0,0), new Point(-1,0), new Point(0,-1), new Point(-1,-1)}, 1),
+    DOND (new Point[] {new Point(0,0), new Point(-1,0), new Point(0,-1), new Point(-1,-1),new Point(-2,0),new Point(0,-2), new Point(-2,-1),new Point(-1,-2),new Point(-2,-2)}, 1);
+
 
     private final Point[] tiles;
     private final int difficulty;
