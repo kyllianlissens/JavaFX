6640c63cceee6dae26e5c5e7fcc048bbf16835b1
Gevzzz <gevorg.minasyan15@gmail.com>
2021-01-07 19:10:03 +0100

    Added enums

--- src/main/java/com/company/BlockShape.java
+++ src/main/java/com/company/BlockShape.java
@@ -6,12 +6,12 @@ public enum BlockShape {
 
     BIG_L (new Point[]{new Point(0,0), new Point(0,-1), new Point(0,-2), new Point(1,-2) }, 3),
     SMALL_L (new Point[] {new Point(0,0), new Point(0,-1), new Point(1,1)}, 1),
-    LIJNH (new Point[] {new Point(0,0), new Point(1,0), new Point(2,0)}, 1),
-    LIJNV (new Point[] {new Point(0,0), new Point(0,-1),  new Point(0,1)}, 1),
-    RB_L (new Point[] {new Point(0,0), new Point(-1,0), new Point(-2,0), new Point(-2,-1)}, 1),
-    RS_L (new Point[] {new Point(0,0), new Point(-1,0), new Point(1,-1)}, 1),
-    TOPT (new Point[] {new Point(0,0), new Point(-1,0), new Point(0,-1), new Point(-1,-1)}, 1),
-    DOPD (new Point[] {new Point(0,0), new Point(-1,0), new Point(0,-1), new Point(-1,-1),new Point(-2,0),new Point(0,-2), new Point(-2,-1),new Point(-1,-2),new Point(-2,-2)}, 1);
+    LINE_HORIZONTAL (new Point[] {new Point(0,0), new Point(1,0), new Point(2,0)}, 1),
+    LINE_VERTICAL (new Point[] {new Point(0,0), new Point(0,-1),  new Point(0,1)}, 1),
+    REVERSERD_BIG_L (new Point[] {new Point(0,0), new Point(-1,0), new Point(-2,0), new Point(-2,-1)}, 1),
+    REVERSERD_SMALL_L (new Point[] {new Point(0,0), new Point(-1,0), new Point(1,-1)}, 1),
+    TWO_ON_TWO (new Point[] {new Point(0,0), new Point(-1,0), new Point(0,-1), new Point(-1,-1)}, 1),
+    THREE_ON_THREE (new Point[] {new Point(0,0), new Point(-1,0), new Point(0,-1), new Point(-1,-1),new Point(-2,0),new Point(0,-2), new Point(-2,-1),new Point(-1,-2),new Point(-2,-2)}, 1);
 
     private final Point[] tiles;
     private final int difficulty;
