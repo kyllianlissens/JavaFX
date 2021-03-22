94b2f41841eb0eacca44474ec05925c2441c7ad8
Gevzzz <gevorg.minasyan15@gmail.com>
2021-03-22 17:29:43 +0100

    settings optimize

--- src/main/java/be/kdg/java/view/settings/SettingsView.java
+++ src/main/java/be/kdg/java/view/settings/SettingsView.java
@@ -25,6 +25,7 @@ public class SettingsView extends GridPane {
     private TextField columnInput;
 
     private Button backButton;
+    private Button saveButton;
     private HBox buttonHBox;
 
     public SettingsView(){
@@ -49,6 +50,8 @@ public class SettingsView extends GridPane {
 
         this.buttonHBox = new HBox(75);
         this.backButton = new Button("Back");
+        this.saveButton = new Button("Save");
+
 
 
     }
@@ -60,7 +63,7 @@ public class SettingsView extends GridPane {
         this.setPadding(new Insets(20, 20, 20, 20));
 
 
-        this.soundSettings.setAlignment(Pos.CENTER);
+        this.soundSettings.setAlignment(Pos.CENTER_RIGHT);
         this.soundSettings.getChildren().addAll(this.musicButton);
 
         this.add(soundSettings, 1,1);
@@ -80,8 +83,8 @@ public class SettingsView extends GridPane {
         this.add(columnSettings,1,3);
 
         this.buttonHBox.setAlignment(Pos.CENTER);
-        this.buttonHBox.getChildren().addAll(this.backButton);
-        this.add(buttonHBox, 3, 3);
+        this.buttonHBox.getChildren().addAll(this.backButton, this.saveButton);
+        this.add(buttonHBox, 1, 4);
         setStyle("-fx-background-image: url(images/wood.jpg);-fx-background-size: cover;");
         this.backButton.setStyle("-fx-padding: 8 15 15 15;\n" +
                 "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
@@ -94,6 +97,18 @@ public class SettingsView extends GridPane {
                 "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
                 "    -fx-font-weight: bold;\n" +
                 "    -fx-font-size: 1.1em;");
+        this.saveButton.setStyle("-fx-padding: 8 15 15 15;\n" +
+                "    -fx-background-insets: 0,0 0 5 0, 0 0 6 0, 0 0 7 0;\n" +
+                "    -fx-background-radius: 8;\n" +
+                "    -fx-background-color: \n" +
+                "        linear-gradient(from 0% 93% to 0% 100%, #a34313 0%, #903b12 100%),\n" +
+                "        #9d4024,\n" +
+                "        #d86e3a,\n" +
+                "        radial-gradient(center 50% 50%, radius 100%, #d86e3a, #c54e2c);\n" +
+                "    -fx-effect: dropshadow( gaussian , rgba(0,0,0,0.75) , 4,0,0,1 );\n" +
+                "    -fx-font-weight: bold;\n" +
+                "    -fx-font-size: 1.1em;");
+        this.musicButton.setStyle("-fx-fill: FIREBRICK;\n" + "  -fx-font-weight: bold;\n" + "  -fx-effect: dropshadow( gaussian , rgba(255,255,255,0.5) , 0,0,0,1 ); -fx-font-size:18px;");
 
 
 
